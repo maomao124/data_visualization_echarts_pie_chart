803ebd3165bcf8a1b62b6000f9d14ae95a9844b2
mao <1296193245@qq.com>
2023-06-14 15:17:44 +0800

    data_visualization_echarts_pie_chart

---
+++ src/main/java/mao/data_visualization_echarts_pie_chart/DataVisualizationEchartsPieChartApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_pie_chart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationEchartsPieChartApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationEchartsPieChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_echarts_pie_chart/DataVisualizationEchartsPieChartApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_echarts_pie_chart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationEchartsPieChartApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
